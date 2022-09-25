4cc802d87b30a1493bff6a10a72fa240e4f9ca72
mbenitez <mbenitez.a@gmail.com>
2022-09-25 16:40:10 -0400

    Initial commit. First screen of the Coding Quiz Challenge

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+// Get references to the #start element
+var startBtn = document.querySelector("#start");
+
+// Start the quiz on #start
+function startQuiz() {
+
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", startQuiz);
