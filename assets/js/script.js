216084f34b85ec0bce8728aa0432e186b109cfce
mbenitez <mbenitez.a@gmail.com>
2022-09-26 18:20:03 -0400

    Implement timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,77 @@
 
-// Get references to the #start element
+// Get reference to the #start element (Start Quiz button)
 var startBtn = document.querySelector("#start");
+// Get reference to the h2 element where the title / questions are posted
 var quizHeader = document.querySelector(".card-h2");
+// Create timer
+var timerEl = document.getElementsByClassName("countdown")[0];
+// Countdown: starts at 75 seconds
+var timeLeft;
+// Number of penalty seconds for a wrong answer 
+var decrement = 10;
+// Quiz has not started or is ongoing
+var done = 0; 
 var score = 0;
+// Keep track of whether there's been any wrong answers
+var wrongAnswer = 0; // Correct answer
+
+function countdown(quizDone) {
+    timeLeft = 75; // Quiz will last 75 seconds
+    timerEl.textContent = "Time: " + timeLeft--;
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 0) {
+            if (done === 0) { // If quiz is ongoing update timer
+                timerEl.textContent = "Time: " + timeLeft;
+                timeLeft--;
+            }
+            else { // Quiz has finished
+                timerEl.textContent = "Time: " + timeLeft;
+                // Stops execution of action at set interval
+                clearInterval(timeInterval);
+            }
+        } else if (timeLeft === 0) { // User ran out of time to complete the quiz
+            timerEl.textContent = "Time: " + timeLeft;
+            // Stops execution of action at set interval
+            clearInterval(timeInterval);
+            finalScreen(wrongAnswer); // Assuming there's a wrong answer!!!
+        }
+    }, 1000);
+}
+
+// Helper function to update the question
+function setQuizHeader(newText) {
+    quizHeader.textContent = newText;
+}
+
+// Display user's scores either when completing the quiz or time is up
+function finalScreen (answer) {
+    // There are no more questions or time ran out, print final results
+    setQuizHeader("All Done!");
+    resetContent(answer);
+    var newH2 = document.createElement("h2");
+    newH2.textContent = "Your final score is " + score;
+    var quizBody = document.querySelector(".card-body");
+    quizBody.appendChild(newH2);
+
+}
 
 // Start the quiz on #start
 function startQuiz() {
     var numOfQuestions = 5;
     var nextQuestion = 0;
-    timer = 75000;
     // Remove text from welcome screen
-    p1.textContent = p2.textContent = "";
+    resetContent(nextQuestion);
     startBtn.textContent = "";
     startBtn.setAttribute("style", "visibility:hidden");
 
+    // Start the quiz timer
+    countdown(done);
+    // Update welcome screen with first question
     displayQuestion(nextQuestion);
 
-    function setQuizHeader(newText) {
-        quizHeader.textContent = newText;
-    }
 
+    // Function to display the questions
     function displayQuestion(question) {
         // Update numOfQuestions in startQuiz after adding more question/answer pairs below
         var questions = ["Commonly used data types DO NOT include:",
@@ -38,7 +90,7 @@ function startQuiz() {
         // Display next answer
         displayAnswers(answers[question]);
     }
-    function createButtons() { }
+
     function displayAnswers(ans) {
         var body = document.body;
         // Declare the style for the answer's buttons
@@ -58,28 +110,23 @@ function startQuiz() {
         }
 
         // Create event listeners for each answer button
-        var ans1 = quizBody.children[2];
-        //console.log(ans1);
+        var ans1 = quizBody.children[0];
         ans1.addEventListener("click", function (event) {
-            //event.preventDefault();
             ans1.setAttribute("style", buttonStyleOnClick);
             verifyAnswer(ans1.textContent);
         });
-        var ans2 = quizBody.children[3];
+        var ans2 = quizBody.children[1];
         ans2.addEventListener("click", function (event) {
-            //event.preventDefault();
             ans2.setAttribute("style", buttonStyleOnClick);
             verifyAnswer(ans2.textContent);
         });
-        var ans3 = quizBody.children[4];
+        var ans3 = quizBody.children[2];
         ans3.addEventListener("click", function (event) {
-            //event.preventDefault();
             ans3.setAttribute("style", buttonStyleOnClick);
             verifyAnswer(ans3.textContent);
         });
-        var ans4 = quizBody.children[5];
+        var ans4 = quizBody.children[3];
         ans4.addEventListener("click", function (event) {
-            //event.preventDefault();
             ans4.setAttribute("style", buttonStyleOnClick);
             verifyAnswer(ans4.textContent);
         });
@@ -87,12 +134,17 @@ function startQuiz() {
 
     function verifyAnswer(userAnswer) {
         var correctAnswerIndex = ["3. alerts", "4. console log", "2. curly brackets", "4. all of the above", "3. quotes"];
-        var wrongAnswer = 0; // Correct answer
         console.log("userAnswer = " + userAnswer);
         console.log("correctAnswer = " + correctAnswerIndex[nextQuestion])
         if (userAnswer != correctAnswerIndex[nextQuestion++]) {
             wrongAnswer = 1; // Wrong answer
-            timer = timer - 10000;
+            if (timeLeft > 10) {
+                timeLeft = timeLeft - decrement;
+            }
+            else {
+                finalScreen(wrongAnswer);
+                done = 1; // Quiz is finished
+            }
             console.log("Wrong answer");
             var quizFooter = document.querySelector(".card-footer");
             var newhr = document.createElement("hr");
@@ -105,33 +157,28 @@ function startQuiz() {
         } else {
             score++;
         }
-        if (nextQuestion >= numOfQuestions) {
-        // There are no more questions, print final results
-            setQuizHeader("All Done!");
-            resetContent(wrongAnswer);
-            var newH2 = document.createElement("h2");
-            newH2.textContent = "Your final score is " + score;
-            var quizBody = document.querySelector(".card-body");
-            quizBody.appendChild(newH2);
-        } else {
+        if (nextQuestion >= numOfQuestions) { // Reached the end of the quiz
+            finalScreen(wrongAnswer);
+            done = 1; // Quiz is finished
+        } else { // Reset screen and display next question
         resetContent(wrongAnswer);
         displayQuestion(nextQuestion);
         }
     }
-    function resetContent(answer) {
-        var quizBody = document.getElementsByClassName("card-body")[0];
-        //console.log(quizBody.children)[2];
-        quizBody.removeChild(quizBody.children[5]);
-        quizBody.removeChild(quizBody.children[4]);
-        quizBody.removeChild(quizBody.children[3]);
-        quizBody.removeChild(quizBody.children[2]);
-        if (answer) {
-            var quizFooter = document.querySelector(".card-footer");
-            quizFooter.removeChild(quizFooter.children[2]);
-            quizFooter.removeChild(quizFooter.children[1]);
+} // end of StartQuiz
+
+function resetContent(answer) {
+    var quizBody = document.getElementsByClassName("card-body")[0];
+    while (quizBody.firstChild) {
+        quizBody.removeChild(quizBody.firstChild);
+    }
+    if (answer) {
+        var quizFooter = document.querySelector(".card-footer");
+        while (quizFooter.firstChild) {
+            quizFooter.removeChild(quizFooter.firstChild);
         }
     }
-} // end of StartQuiz
+}
 
     // Add event listener to 'Start Quiz' button
     startBtn.addEventListener("click", startQuiz);
