dba05b824db7f943dd714e51d31a64ee4631e7d0
mbenitez <mbenitez.a@gmail.com>
2022-09-25 22:15:33 -0400

    Enhance quiz formatting: change button color on hover/click, indicate when an answer is wrong

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 
 // Get references to the #start element
 var startBtn = document.querySelector("#start");
-var quizHeader = document.querySelector(".card-header");
+var quizHeader = document.querySelector(".card-h2");
 
 // Start the quiz on #start
 function startQuiz() {
     var numOfQuestions = 5;
+    timer = 75000;
     // Remove text from welcome screen
     p1.textContent = p2.textContent = "";
+    startBtn.textContent = "";
+    startBtn.setAttribute("style", "visibility:hidden");
 
     //for (q = 0; q < numOfQuestions; q++) {
-        displayQuestions(0);
+    displayQuestions(0);
     //}
 
     function displayQuestions(question) {
@@ -34,19 +37,61 @@ function startQuiz() {
 
     function displayAnswers(ans) {
         var body = document.body;
-        //
+        // Declare the style for the answer's buttons
         var buttonStyle = "background-color:#351c75;color:white;text-align:left;padding-bottom:10px;border-radius:10px;display:block;font-size: 22px;line-height: 22px;margin: 16px 16px 16px 20px;padding: 14px 34px;width:250px;"
-            
+        var buttonStyleOnClick = "background-color:#bb5be6;color:white;text-align:left;padding-bottom:10px;border-radius:10px;display:block;font-size: 22px;line-height: 22px;margin: 16px 16px 16px 20px;padding: 14px 34px;width:250px;"
+
         // Select <div> element with class card-body
         var quizBody = document.getElementsByClassName("card-body")[0];
         for (i = 0; i < ans.length; i++) {
             // Create new button to display a possible answer
             var newbtn = document.createElement("button");
             // Add answer content to the button
-            newbtn.textContent = i+1 + '. ' + ans[i];
+            newbtn.textContent = i + 1 + '. ' + ans[i];
             newbtn.setAttribute("style", buttonStyle);
+            newbtn.className = "btn";
             quizBody.appendChild(newbtn);
-         }
+        }
+
+        // Create event listeners for each answer button
+        var ans1 = quizBody.children[2];
+        console.log(ans1);
+        ans1.addEventListener("click", function (event) {
+            ans1.setAttribute("style",buttonStyleOnClick);
+            verifyAnswer(ans1.textContent);
+         });
+        var ans2 = quizBody.children[3];
+        ans2.addEventListener("click", function (event) {
+            ans2.setAttribute("style",buttonStyleOnClick);
+             verifyAnswer(ans2.textContent);
+        });
+        var ans3 = quizBody.children[4];
+        ans3.addEventListener("click", function (event) {
+            ans3.setAttribute("style",buttonStyleOnClick);
+            verifyAnswer(ans3.textContent);
+        });
+        var ans4 = quizBody.children[5];
+        ans4.addEventListener("click", function (event) {
+            ans4.setAttribute("style",buttonStyleOnClick);
+            verifyAnswer(ans4.textContent);
+        });
+    }
+
+    function verifyAnswer(userAnswer) {
+        var correctAnswerIndex = ["3. alerts", "4. console log", "2. curly brackets", "4. all of the above", "3. quotes"];
+        console.log("userAnswer = " + userAnswer);
+        if (userAnswer != correctAnswerIndex[0]) {
+            timer = timer - 10000;
+            console.log("Wrong answer");
+            var quizFooter = document.querySelector(".card-footer");
+            var newhr = document.createElement("hr");
+            newhr.setAttribute("width","100%");
+            var newH2 = document.createElement("h2");
+            newH2.textContent = "Wrong!";
+            newH2.setAttribute("style","color:grey;text-align:left;font-size:20px");
+            quizFooter.appendChild(newhr);
+            quizFooter.append(newH2);
+        }
     }
 }
 
